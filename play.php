2a7a1f25d9af58b93e0ff6e5181577341c96fee6
jlopez1423 <jlopez46@ucsc.edu>
2017-11-28 19:56:38 -0800

    Getting controller to work with doctorine

--- play.php
+++ play.php
@@ -12,9 +12,6 @@ require_once __DIR__.'/app/AppKernel.php';
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
 $kernel->boot();
 
 $container = $kernel->getContainer();
@@ -24,9 +21,16 @@ $container->set( 'request', $request );
 //All the set up is done!
 
 
-$templating = $container->get ( 'templating' );
+use Yoda\EventBundle\Entity\Event;
 
-echo $templating->render(
-    'EventBundle:Default:index.html.twig',
-    array( 'name' => 'Vader', 'count' => 3 )
-);
+$event = new Event();
+$event->setName( 'Darth\'s surprise birthday party' );
+$event->setLocation( 'Deathstar' );
+$event->setTime( new \DateTime( 'tomorrow noon' ) );
+//$event->setDetails( 'Ha Darth HATES surprises!!!' );
+
+$em = $container->get( 'doctrine' )->getManager();
+
+$em->persist( $event );
+
+$em->flush();
