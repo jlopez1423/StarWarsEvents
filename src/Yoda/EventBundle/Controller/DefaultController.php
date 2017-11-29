c2fa651d719001b62ff7a2748f5548c61e84f3b8
jlopez1423 <jlopez46@ucsc.edu>
2017-11-28 16:49:04 -0800

    Using repsonse object to render json repsonse

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,13 +3,28 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction( $firstName, $count )
     {
 
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName));
+        $data = array(
 
+            'count' => $count,
+            'firstName' => $firstName,
+            'ackbar' => 'It\'s a traaaapp!'
+        );
+
+        $json = json_encode( $data );
+
+        //Make sure we get the correct content type header.
+
+        $response = new Response( $json );
+
+        $response->headers->set('Content-Type', 'application/json' );
+
+        return $response;
     }
 }
