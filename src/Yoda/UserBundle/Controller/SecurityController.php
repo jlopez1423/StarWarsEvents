4e9d25d92aaab1d867e02dda9298fe46e3047870
jlopez1423 <jlopez46@ucsc.edu>
2017-12-01 15:21:04 -0800

    Added loginAction to SecurityController

---
+++ src/Yoda/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jlo
+ * Date: 12/1/17
+ * Time: 1:16 PM
+ */
+
+namespace Yoda\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route( "/login", name="login_form")
+     * @Template
+     */
+    public function loginAction(Request $request)
+    {
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        );
+
+    }
+}
---
+++ src/Yoda/UserBundle/UserBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jlo
+ * Date: 12/1/17
+ * Time: 1:11 PM
+ */
+
+namespace Yoda\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+
+}
