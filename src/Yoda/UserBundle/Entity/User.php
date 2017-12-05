c408a208de6ddc1a71e582e5abca846963ca43db
Jose Lopez <jalopez1423@gmail.com>
2017-12-05 11:19:51 -0800

    Edit: Server side validation

--- src/Yoda/UserBundle/Entity/User.php
+++ src/Yoda/UserBundle/Entity/User.php
@@ -4,6 +4,8 @@ namespace Yoda\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Serializable;
 
 
@@ -12,6 +14,8 @@ use Serializable;
  *
  * @ORM\Table(name="yoda_user")
  * @ORM\Entity(repositoryClass="Yoda\UserBundle\Entity\UserRepository")
+ * @UniqueEntity( fields={"email"}, message="That email is taken!")
+ * @UniqueEntity( fields={"username"}, message="That username is taken!")
  */
 class User implements AdvancedUserInterface, Serializable
 {
@@ -27,6 +31,8 @@ class User implements AdvancedUserInterface, Serializable
     /**
      * @var string
      *
+     * @Assert\NotBlank( message="Put in a username you rebel scum!" )
+     * @Assert\Length( min=3, minMessage="Give us at least 3 characters")
      * @ORM\Column(name="username", type="string", length=255)
      */
     private $username;
@@ -42,6 +48,11 @@ class User implements AdvancedUserInterface, Serializable
     /**
      * Just stores the plain password temporarily!
      *
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *  pattern = "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/",
+     *  message="Use 1 upper case letter, 1 lower case letter, and 1 number"
+     * )
      * @var string
      */
     private $plainPassword;
@@ -64,6 +75,8 @@ class User implements AdvancedUserInterface, Serializable
     /**
      * @var string
      *
+     * @Assert\NotBlank()
+     * @Assert\Email
      * @ORM\Column(type="string", length=255)
      */
     private $email;
