551b9220e3f5d58b21ffebe418bc42708b63e748
Jose Lopez <jose@Joses-MacBook-Pro.local>
2018-02-17 18:13:53 -0800

    Added: method to return response type.

--- src/Yoda/EventBundle/Controller/EventController.php
+++ src/Yoda/EventBundle/Controller/EventController.php
@@ -3,10 +3,10 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 use Yoda\EventBundle\Entity\Event;
 use Yoda\EventBundle\Form\EventType;
@@ -256,22 +256,10 @@ class EventController extends Controller
         $em->persist($event);
         $em->flush();
 
-        if ($format == 'json'){
-            $data = array(
-                'attending' => true
-            );
-            $response = new Response(json_encode($data));
-            return $response;
-        }
-
-        $url = $this->generateUrl('event_show', array(
-            'slug' => $event->getSlug()
-        ));
-
-        return $this->redirect($url);
+        return $this->createAttendingResponse($event, $format);
     }
 
-    public function unattendAction($id)
+    public function unattendAction($id, $format)
     {
         $em = $this->getDoctrine()->getManager();
         $event = $em->getRepository('EventBundle:Event')->find($id);
@@ -288,13 +276,27 @@ class EventController extends Controller
         $em->persist($event);
         $em->flush();
 
+        return $this->createAttendingResponse($event, $format);
+
+    }
+
+    private function createAttendingResponse(Event $event, $format)
+    {
+
+        if ($format == 'json') {
+            $data = array(
+                'attending' => $event->hasAttendee($this->getUser()),
+            );
+            $response = new JsonResponse($data);
+            return $response;
+        }
+
         $url = $this->generateUrl('event_show', array(
             'slug' => $event->getSlug()
         ));
 
         return $this->redirect($url);
     }
-
     /**
      * Creates a form to delete a Event entity by id.
      *
