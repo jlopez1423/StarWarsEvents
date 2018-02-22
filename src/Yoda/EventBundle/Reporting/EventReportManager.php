2cbce33b799d871e866e31c6fe920ecaa60643e9
Jose <jalopez1423@gmail.com>
2018-02-21 20:56:58 -0800

    Added: urls to csv

--- src/Yoda/EventBundle/Reporting/EventReportManager.php
+++ src/Yoda/EventBundle/Reporting/EventReportManager.php
@@ -9,14 +9,18 @@
 namespace Yoda\EventBundle\Reporting;
 
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Routing\Router;
 
 class EventReportManager
 {
     private $em;
 
-    public function __construct(EntityManager $em)
+    private $router;
+
+    public function __construct(EntityManager $em, Router $router)
     {
         $this->em = $em;
+        $this->router = $router;
     }
 
     public function getRecentlyUpdatedReport()
@@ -30,7 +34,12 @@ class EventReportManager
             $data = array(
                 $event->getId(),
                 $event->getName(),
-                $event->getTime()->format('Y-m-d H:i:s')
+                $event->getTime()->format('Y-m-d H:i:s'),
+                $this->router->generate(
+                    'event_show',
+                    array('slug' => $event->getSlug()),
+                    true
+                ),
             );
 
             $rows[] = implode(',', $data);
